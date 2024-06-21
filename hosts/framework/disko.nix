fce16c241d8db1c1087c1782efcfb83ac711c06f
Susan <susan@susan.lol>
2024-06-21 14:45:13 +0000

    Update disko.nix

--- hosts/framework/disko.nix
+++ hosts/framework/disko.nix
@@ -28,7 +28,7 @@
                 settings = {
                   # if you want to use the key for interactive login be sure there is no trailing newline
                   # for example use `echo -n "password" > /tmp/secret.key`
-                  keyFile = "/tmp/password.key";
+                  passwordFile = "/tmp/password.key";
                   allowDiscards = true;
                 };
                 content = {
